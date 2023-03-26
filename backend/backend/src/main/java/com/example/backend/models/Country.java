bd27ec91d45d1ddfdb38720c7f718de552cae74f
123Olga1233 <kurzinaolga1612@gmail.com>
2023-03-26 15:27:57 +0300

    lab

--- backend/backend/src/main/java/com/example/backend/models/Country.java
+++ backend/backend/src/main/java/com/example/backend/models/Country.java
@@ -18,6 +18,7 @@ public class Country {
     @Column(name = "id", updatable = false, nullable = false)
     public long id;
 
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, unique = true)
     public String name;
+
 }
